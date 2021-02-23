a57b93a85c02d398d10d4a1ecd1dc0f3058edaf5
Valee <valeriansalim@gmail.com>
2021-02-23 23:03:24 +0700

    added background image and customized loading bar initial state

--- src/components/LoadingBar.js
+++ src/components/LoadingBar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './LoadingBar.css';
 
 const LoadingBar = ({ targetTime, remainingTime }) => {
-	let barPercentage = targetTime ? 1-remainingTime/targetTime : 0;
+	let barPercentage = targetTime ? 1-remainingTime/targetTime : 1;
 	document.documentElement.style.setProperty('--bar-width', `${100*barPercentage}vw`);
 	document.documentElement.style.setProperty('--movbar-glow', targetTime ? 'inline-block' : 'none');
 	return (
